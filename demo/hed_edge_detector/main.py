2f280b92eaa68301bd17b6937da940efb34c55bc
yunlongdong <yunlongdong@outlook.com>
2020-03-27 20:30:03 +0800

    adding hed demo

---
+++ demo/hed_edge_detector/main.py
@@ -0,0 +1,46 @@
+from cnnumpy import read_onnx
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import io, transform
+from time import time
+
+
+# size to be 8x 
+def makesize8(img):
+    h, w = img.shape[:2]
+    w = (w//32 + 0)*32
+    h = (h//32 + 0)*32
+    img = transform.resize(img, (h, w), preserve_range=True)
+    return img
+
+def normal(img):
+    img = img.copy()
+    img[:, :, 0] -= 104
+    img[:, :, 1] -= 117
+    img[:, :, 2] -= 123
+    return img.astype('float32')
+
+img =  io.imread('test.jpg')
+img = makesize8(img)
+x = normal(img).transpose(2, 0, 1)[None, :, :, :].copy()
+
+
+# 2 files needed, hed.txt, hed.npy
+net = read_onnx('hed')
+y = net(x)
+start = time()
+y = net(x)
+print('craft detect time:', time()-start)
+
+plt.subplot(121)
+plt.imshow(img.astype('uint8'))
+# edge map
+plt.subplot(122)
+plt.imshow(y[0, 0, :, :])
+plt.title('edge map')
+
+plt.show()
+
+
+
+
--- demo/readme.md
+++ demo/readme.md
@@ -6,3 +6,9 @@ Demo for scene text detector with model [CRAFT](https://github.com/clovaai/CRAFT
 Run ```python main.py``` inside folder ```craft_text_detector```. 3 files (same level folder with ```main.py```) are needed (craft.txt, [craft.npy](), [craft_np.npy]())
 The detected result is shown as following: 
 ![](https://raw.githubusercontent.com/Image-Py/cnnumpy/master/demo/craft_text_detector/rst.png)
+
+## HED edge detector
+Demo for edge detector with model HED. Run ```python main.py``` inside folder ```hed_edge_detector```. 2 files (same level folder with ```main.py```) are needed (hed.txt, [hed.npy]())
+The detected result is shown as following: 
+![](https://raw.githubusercontent.com/Image-Py/cnnumpy/master/demo/hed_edge_detector/rst.png)
+
