318b34c0ca41ec43ed1c0cfee2061f90b6aa00d0
yunlongdong <yunlongdong@outlook.com>
2020-03-29 22:04:32 +0800

    adding mobilenet

---
+++ demo/mobilenet-v1/main.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('../../')
+import numpy as np
+from npcnn import read_onnx, resize
+from imagenet_labels import classes
+from skimage import io
+from matplotlib import pyplot as plt
+from time import time
+
+img = io.imread('test.jpg')
+x = (img/255.0).transpose(2, 0, 1)
+x = x[None, :, :, :].astype('float32')
+x = resize(x, (224, 224))
+
+net = read_onnx('mobile')
+print('load done!')
+
+net(x)
+start = time()
+y = net(x)
+print('npcnn mobilenet-v1 time:', time()-start)
+y = np.argmax(y, axis=-1)
+rst = classes[y[0]]
+
+print('result:', rst)
+plt.imshow(img.astype('uint8'))
+plt.title(rst)
+plt.show()
